09d58e43807e4af135d0a9cacb3155c72e02b2c4
zero <45618003+0011000000110010@users.noreply.github.com>
2024-03-07 01:13:07 +0800

    Add files via upload

--- src/settings/ui/i18n-template.ts
+++ src/settings/ui/i18n-template.ts
@@ -1,9 +1,6 @@
-import { Setting } from "obsidian";
 import BaseSetting from "./base-setting";
 
 export default class I18n extends BaseSetting {
     main(): void {
-        const I18n = new Setting(this.containerEl);
-        console.log(I18n)
     }
 } 
