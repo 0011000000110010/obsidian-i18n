330620dfeaf973ebd6e0d58060ab6b501a8ffe4d
曲淡歌 <623009848@qq.com>
2024-09-22 08:03:02 +0000

    update zh-cn/auto_pr.py.
    Signed-off-by: 曲淡歌 <623009848@qq.com>

--- zh-cn/auto_pr.py
+++ zh-cn/auto_pr.py
@@ -49,7 +49,7 @@ def git_command(command):
         raise
 
 def branch_exists(branch_name):
-    result = git_command(f"git ls-remote --heads origin "{branch_name}"")
+    result = git_command(f"git ls-remote --heads origin '{branch_name}'')
     return bool(result.stdout.strip())
 
 def create_pull_request(pr_type, info, issue_number, branch_name=None):
@@ -176,10 +176,10 @@ def process_issues():
                 
                 print(f'已保存: {full_file_path}')
                 
-                git_command(f"git checkout -b "{branch_name}"")
+                git_command(f"git checkout -b '{branch_name}'")
                 git_command(f"git add {file_path}")
                 git_command(f'git commit -m "Add {info["id"]} version {info["pluginVersion"]}"')
-                git_command(f'git push -u origin "{branch_name}"')
+                git_command(f"git push -u origin '{branch_name}'")
                 
                 create_pull_request(pr_type, info, issue['number'])
                 
@@ -201,10 +201,10 @@ def process_issues():
                 branch_name = f"ignore-{plugin_id}"
                 
                 try:
-                    git_command(f"git checkout -b "{branch_name}"")
+                    git_command(f"git checkout -b '{branch_name}'")
                 except subprocess.CalledProcessError:
                     print(f"分支 {branch_name} 已存在，切换到该分支")
-                    git_command(f"git checkout "{branch_name}"")
+                    git_command(f"git checkout '{branch_name}'")
                 
                 git_command("git add zh-cn/ignore.json")
                 try:
@@ -216,7 +216,7 @@ def process_issues():
                     else:
                         raise
 
-                git_command(f"git push -u origin "{branch_name}"")
+                git_command(f"git push -u origin '{branch_name}'")
 
                 # 等待一段时间，确保分支已经同步到远程
                 print("等待 5 秒，确保分支同步到远程...")
